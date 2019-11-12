9ab8b50324f5fcf666adba54ef73ad2fd16b74d5
Yuri-Cavalcanti <yuri.cflive@gmail.com>
2019-11-12 20:38:55 -0200

    porra

--- scriptPat.py
+++ scriptPat.py
@@ -4,9 +4,9 @@ import git
 #for line in sys.stdin:
  #   local_ref, local_sha1, remote_ref, remote_sha1 = line.strip().split()
  #   print(sys.stdin.file)
-repo = Repo
+repo = git.repo(sys.stdin.local_ref)
 
 for commit in repo.iter_commits('master'):
-	print(commit)  
+	print(commit.file)  
 
 
