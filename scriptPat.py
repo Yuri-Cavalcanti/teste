f0e1e6ddcc94db0d46a6b80eded844fb7107b553
Yuri-Cavalcanti <yuri.cflive@gmail.com>
2019-11-12 18:06:28 -0200

    porra

--- scriptPat.py
+++ scriptPat.py
@@ -1,22 +1,5 @@
-
-import os
-import utils
 import sys
-from utils import debug, warn
-from git import git, diff_tree, file_exists
-from config import git_config
-from git_attrs import git_attribute
 
-changes = diff_tree('-r', old_rev, new_rev)
-nomeArquivos = []
-
-for item in changes:
-    (old_mode, new_mode, old_sha1, new_sha1, status, filename) = item
-     debug('diff-tree entry: %s %s %s %s %s %s'
-        % (old_mode, new_mode, old_sha1, new_sha1, status, filename),
-            level=5)
-    nomeArquivos.append(filename)
-
-
-
-    print(filename)
+for line in sys.stdin:
+    local_ref, local_sha1, remote_ref, remote_sha1 = line.strip().split()
+    print(local_sha1.fileName)
