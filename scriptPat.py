7e2c8cc62b24ff5e12a1d7feaf5ef48ba54cb4be
Yuri-Cavalcanti <yuri.cflive@gmail.com>
2019-11-12 17:33:27 -0200

    asdadsadsad

--- scriptPat.py
+++ scriptPat.py
@@ -1,6 +1,19 @@
-import sys
 
+import os
+import utils
+from utils import debug, warn
+from git import git, diff_tree, file_exists
+from config import git_config
+from git_attrs import git_attribute
 
-arquivo = open(sys.argv[1])
+changes = diff_tree('-r', old_rev, new_rev)
+nomeArquivos = []
 
-print(arquivo)
+for item in changes:
+    (old_mode, new_mode, old_sha1, new_sha1, status, filename) = item
+     debug('diff-tree entry: %s %s %s %s %s %s'
+        % (old_mode, new_mode, old_sha1, new_sha1, status, filename),
+            level=5)
+    nomeArquivos.append(filename)
+
+    print(filename)
