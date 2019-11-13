d0d3a318ced234af0467131e66065f76851c51ba
Yuri-Cavalcanti <yuri.cflive@gmail.com>
2019-11-13 15:02:07 -0200

    ue

--- scriptPat.py
+++ scriptPat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 def create():
-    Documents = os.popen("git diff --cached --name-status | cut -f2").read()
+    Documents = os.popen("git diff --cached --name-status | cut -f2").read().splitlines()
     for name in Documents:
         print(name)
     return sys_msg
