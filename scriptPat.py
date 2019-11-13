df2cbb05a57bf5bf3ced4e728cd67644ed3940a3
Yuri-Cavalcanti <yuri.cflive@gmail.com>
2019-11-13 15:55:55 -0200

    ue

--- scriptPat.py
+++ scriptPat.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 def Compare(path):
     div = path.split("/")
-    print(div[len(div) -1])
     if div[len(div) - 1] == "teste.txt":
         return 1
     else:
         return 0
+
+
+
+
 
